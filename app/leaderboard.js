50349264f6470d510a2e07ff09e0129d4e2fcb33
Dan <danli@hawaii.edu>
2017-03-02 09:16:53 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,3 +1,31 @@
 /**
  * Created by danli on 2/28/2017.
  */
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers ({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'selectedClass': function() {
+      let playerId = this._id;
+      let selectedPlayer = Session.get('selectedPlayer');
+      if (playerId == selectedPlayer) {
+        return "selected";
+      }
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      let playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+}
+
+if(Meteor.isServer){
+
+}
+
